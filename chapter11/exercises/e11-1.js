ec1d4d45d4d53a111a9031d0b264837361c4a5f7
Will <138933808+wwarre309@users.noreply.github.com>
2023-07-12 11:05:41 -0400

    will check permissions

--- chapter11/exercises/e11-1.js
+++ chapter11/exercises/e11-1.js
@@ -15,5 +15,5 @@ a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
-
+let workGithub = 'humn'
 let arr = ['Elocution', 21, 'Clean teeth', 100];
