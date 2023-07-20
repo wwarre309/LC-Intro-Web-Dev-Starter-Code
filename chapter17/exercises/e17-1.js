03f7133313179c1e8db62e6eca2a4eb172171881
Will <138933808+wwarre309@users.noreply.github.com>
2023-07-20 17:03:00 -0400

    Chapter18 exercise

--- chapter17/exercises/e17-1.js
+++ chapter17/exercises/e17-1.js
@@ -7,3 +7,55 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+
+function gradeLabs(labs) {
+    for (let i=0; i < labs.length; i++) {
+      try{ 
+        let lab = labs[i];
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+      } catch (err) {
+        console.log("Error Thrown")}
+    }
+  }
+  
+  let studentLabs = [
+    {
+      student: 'Carly',
+      runLab: function (num) {
+          return Math.pow(num, num);
+      }
+    },
+    {
+      student: 'Erica',
+      runLab: function (num) {
+          return num * num;
+      }
+    }
+  ];
+  
+  gradeLabs(studentLabs);
+  
+  let studentLabs2 = [
+     {
+        student: 'Blake',
+        myCode: function (num) {
+           return Math.pow(num, num);
+        }
+     },
+     {
+        student: 'Jessica',
+        runLab: function (num) {
+           return Math.pow(num, num);
+        }
+     },
+     {
+        student: 'Mya',
+        runLab: function (num) {
+           return num * num;
+        }
+     }
+  ];
+  
+  gradeLabs(studentLabs2);
--- chapter18/exercises/e18.js
+++ chapter18/exercises/e18.js
@@ -1,10 +1,51 @@
 // Define your Book class here:
-
-
-// Define your Manual and Novel classes here:
-
-
-// Declare the objects for exercises 2 and 3 here:
-
-
-// Code exercises 4 & 5 here:
+class Book {
+    constructor(title, author, copyrightDate, ISBN, pages, checkoutAmount, discardedYet) {
+      this.title = title;
+      this.author = author;
+      this.copyrightDate = copyrightDate;
+      this.ISBN = ISBN;
+      this.pages = pages;
+      this.checkoutAmount= checkoutAmount;
+      this.discardedYet = discardedYet;
+    }
+    checkout(uses=1) {
+      this.checkoutAmount += uses
+    }
+    
+  }
+  
+  // Define your Manual and Novel classes here:
+  class Manual extends Book{
+    constructor(title, author, copyrightDate, ISBN, pages, checkoutAmount, discardedYet) {
+      super(title, author, copyrightDate, ISBN, pages, checkoutAmount, discardedYet); 
+    }
+    dispose() {
+      if (this.copyrightDate > 5) {
+        this.discardedYet = "Yes";
+      }
+    }
+  }
+  
+  class Novel extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+        super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+     }
+    dispose() {
+      if (this.checkoutAmount > 100) {
+        this.discardedYet = "Yes";
+      }
+    }
+  }
+  // Declare the objects for exercises 2 and 3 here:
+  let novel1 = new Novel("Pride & Prejudice", "Jane Austen", 1813, "1111111111111", 432, 32, "No");
+  // make new variable in "let" for child class to become after intaking parent
+  
+  let manual1 = new Manual("Top Secret Shuttle Building Manual", "Redacted", 2013, "0000000000000", 1147, 1, "No");
+  
+  // Code exercises 4 & 5 here:
+  
+  manual1.dispose()
+  
+  
+  novel1.checkout(5);
