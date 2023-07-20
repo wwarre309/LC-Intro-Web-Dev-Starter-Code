be7a005fe0d316d90f51313ef1af37c0e62a9faf
Will <138933808+wwarre309@users.noreply.github.com>
2023-07-20 17:39:55 -0400

    chapter14 studio

--- chapter14/studio/spec/launchcode.spec.js
+++ chapter14/studio/spec/launchcode.spec.js
@@ -4,5 +4,31 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  it ("Checks the vaule of 'organization'", function() {
+    let result = launchcode.organization;
+    expect(result).toEqual("nonprofit");
+  });
+
+  it ("Checks the value of 'executiveDirector'", function() {
+    let result = launchcode.executiveDirector;
+    expect(result).toEqual("Jeff");
+  });
+  it ("Checks the valur of 'percentageCoolEmployees'", function() {
+    let result = launchcode.percentageCoolEmployees;
+    expect(result).toEqual(100);
+  });
+  it ("Checks the value of 'programsOffered'", function() {
+    let result = launchcode.programsOffered;
+    expect(result[0]).toEqual("LC101");
+    expect(result[1]).toEqual("LaunchCode Women+");
+    expect(result[2]).toEqual("CodeCamp");
+    expect(result.length).toEqual(3);
+  });
+  it ("Provide proper answers for 'launchOutput()'", function() {
+    expect(launchcode.launchOutput(2)).toEqual("Launch!");
+    expect(launchcode.launchOutput(3)).toEqual("Code!");
+    expect(launchcode.launchOutput(5)).toEqual("Rocks!");
+  })
   
 });
+
